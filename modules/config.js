aedcc95120800da1fbe4e79bd7605df16198f340
Devin Alexander <devinsonlyemail@gmail.com>
2026-01-02 21:57:45 +0200

    feat: add finalModule and finalLesson config for course completion webhooks
    - Course1: module6/lesson30 (Blockchain)
    - Course2: module5/lesson14 (AI Mastery)
    - Course3: module8/lesson51 (Advanced Crypto)
    - Also updated Greek course names from Webflow CMS

--- modules/config.js
+++ modules/config.js
@@ -3,17 +3,23 @@ export const Config = {
     'course1': {
       totalLessons: 30,
       name: 'Blockchain Fundamentals',
-      nameGreek: 'Blockchain Kαι Κρυπτονομίσματα Από Το Α-Ω'
+      nameGreek: 'Blockchain Kαι Κρυπτονομίσματα Από Το Α-Ω',
+      finalModule: 'module6',
+      finalLesson: 'lesson30'
     },
     'course2': {
       totalLessons: 14,
       name: 'AI Master Course',
-      nameGreek: 'AI Master Course'
+      nameGreek: 'AI Mastery για τη Νέα Οικονομία',
+      finalModule: 'module5',
+      finalLesson: 'lesson14'
     },
     'course3': {
       totalLessons: 51,
       name: 'Advanced Crypto',
-      nameGreek: 'Advanced Crypto'
+      nameGreek: 'Ανώτερη Εκπαίδευση για Κρυπτονομίσματα',
+      finalModule: 'module8',
+      finalLesson: 'lesson51'
     }
   },
   
